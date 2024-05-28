5b62c8da6e9caf1b839cbe1c1b99157eea72219f
Abdo <ba2662003@gmail.com>
2024-05-28 23:05:39 +0300

    update in icon and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@
 const Footer = () => {
     return (
         <div className="flex items-center justify-center py-4 bg-bgcolor-2">
-            <p className="text-lg font-bold text-white">&copy; 2022
-                <span className="text-primarycolor"> Abdulrahman Ahmed</span> All Right Reserved
-            </p>
+            <div className="container">
+                <p className="text-lg font-bold text-center text-white">&copy; 2022
+                    <span className="text-primarycolor"> Abdulrahman Ahmed</span> All Right Reserved
+                </p>
+            </div>
         </div>
     );
 }
